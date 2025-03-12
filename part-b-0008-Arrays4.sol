46269668aebf7671b4c51faef42d77d0bad28976
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-12 21:25:49 +0300

    Create part-b-0008-Arrays4.sol

---
+++ part-b-0008-Arrays4.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SalaryKeeper {
+
+  // let's initialize some arrays
+
+  uint8[] public ages;
+
+  string[] public titles = ["Manager", "Expert", "Intern"];
+
+  uint16[5] public yearsArray =[2018, 2020, 2022, 2023, 2024];
+
+}
